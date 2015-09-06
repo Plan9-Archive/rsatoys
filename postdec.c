f133f540875a50ca92e37ae4f5abc6020cf88c8d
glenda <glenda@gnot>
2015-09-06 08:55:39 +0000

    fix use of postdec in pipe from preenc

--- postdec.c
+++ postdec.c
@@ -7,22 +7,29 @@ main(int argc,char *argv[])
 	char buf[2];
 	int len;
 	ulong out;
+	int n;
 
 newchunk:
 	/* read and ignore pkcs1.5 padding */
-	read(0,buf,2);
+	n=read(0,buf,2);
+	if(n==1)
+		read(0,buf+1,1);
 	if(strncmp(buf,"02",2)!=0){
 		fprint(2,"padding length error\n");
 		exits(nil);
 	}
 	len=2;
 	while(strncmp(buf,"00", 2) != 0){
-		read(0,buf,2);
+		n=read(0,buf,2);
+		if(n==1)
+			read(0,buf+1,1);
 		len+=2;
 	}
 	/* convert from text-hex to original data */
 	out=0;
-	while(read(0,buf,2)){
+	while(n=read(0,buf,2)){
+		if(n==1)
+			read(0,buf+1,1);
 		out=strtoul(buf,nil,16);
 		write(1,&out,1);
 		len+=2;
